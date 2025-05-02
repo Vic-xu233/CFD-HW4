595a84aaf6915a424166acf57f9135c12a8f40dc
Kiwi Xu <920650757@qq.com>
2025-05-02 22:49:49 +0800

    add main

--- SORmethod.py
+++ SORmethod.py
@@ -1,39 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from mpl_toolkits.mplot3d import Axes3D
-# size (cm)
-Lx = 15.0  # 宽
-Ly = 12.0  # 高
-
-# 网格数
-Nx = 75
-Ny = 60
-
-# 间距ΔX, ΔY
-dx = Lx / (Nx - 1)
-dy = Ly / (Ny - 1)
-beta = dx / dy 
-
-# 松弛因子ω
-omega = 1.8
-
-# 判定收敛
-tolerance = 1e-6
-max_iter = 10000
-
-# 初值
-T = np.full((Nx, Ny), 20.0)  # initial guess = 20°C everywhere
-
-# 边界条件
-T[:, -1] = 100.0  # 上边
-T[:, 0] = 20.0    # 下边
-T[0, :] = 20.0    # 左边
-T[-1, :] = 20.0   # 右边
 
 # SOR迭代
 def SOR(T, omega, beta, max_iter, tolerance):
+    Nx, Ny = T.shape
     for iteration in range(max_iter):
-        T_old = T.copy()
+        T_old = T.copy() 
         for i in range(1, Nx - 1):
             for j in range(1, Ny - 1):
                 T_new = (1 / (2 * (1 + beta**2))) * (
@@ -45,34 +19,83 @@ def SOR(T, omega, beta, max_iter, tolerance):
            print(f"经过{iteration+1} 次迭代收敛，松弛因子 ω = {omega}")
            break
     else:
-        iteration =max_iter
         print("超过最大次数")
-    return T,iteration+1
-
-omega_values = np.arange(1.8, 2.0, 0.05) 
-results = {}
-T_best = {}
-
-for omega in omega_values:
-  T_best[omega],results[omega]=SOR(T, omega, beta, max_iter, tolerance)
-
-best_omega = min(results, key=results.get)
-T = T_best[best_omega]
-print(f"Best ω = {best_omega}, iterations = {results[best_omega]}")
-
+    return iteration+1
+def find_best_omega(omega_values, T, beta, max_iter, tolerance, results):
+    for omega in omega_values:
+        if omega in results:          # 已算过的不重复
+            continue
+        Ti = T.copy()
+        iterations = SOR(Ti, omega, beta, max_iter, tolerance)
+        results[omega] = iterations
+        w_best= min(results, key=results.get)
+    return w_best
 # 可视化
-x = np.linspace(0, Lx, Nx)
-y = np.linspace(0, Ly, Ny)
-X, Y = np.meshgrid(x, y, indexing='ij')
+def draw(Lx,Ly,Nx,Ny,T_final,best_omega):
+    plt.figure(figsize=(8, 5))
+    x = np.linspace(0, Lx, Nx)
+    y = np.linspace(0, Ly, Ny)
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    cp = plt.contourf(X, Y, T_final, 50, cmap='hot')
+    plt.colorbar(cp, label='Temperature (°C)')
+    plt.title(f"2D Heat Conduction (SOR, ω = {best_omega},Nx={Nx},Ny={Ny})")
+    plt.xlabel("x (cm)")
+    plt.ylabel("y (cm)")
+    plt.tight_layout()
+    plt.show()
 
-plt.figure(figsize=(8, 5))
-cp = plt.contourf(X, Y, T, 50, cmap='hot')
-plt.colorbar(cp, label='Temperature (°C)')
-plt.title(f"2D Heat Conduction (SOR, ω = {omega})")
-plt.xlabel("x (cm)")
-plt.ylabel("y (cm)")
-plt.tight_layout()
-plt.show()
+def maincode(Nx,Ny):
+    # 网格划分
+    Lx = 15.0  # 宽
+    Ly = 12.0  # 高
+    dx = Lx / (Nx - 1)
+    dy = Ly / (Ny - 1)
+    beta = dx / dy 
+    # 判定收敛
+    tolerance = 1e-6
+    max_iter = 10000
+
+    # 初值
+    T = np.full((Nx, Ny), 20.0)  # initial guess = 20°C everywhere
+
+    # 边界条件
+    T[:, -1] = 100.0  # 上边
+    T[:, 0] = 20.0    # 下边
+    T[0, :] = 20.0    # 左边
+    T[-1, :] = 20.0   # 右边
+    results = {} #存储每个ω的迭代次数
+
+    #设定松弛因子搜索范围
+    left=1.0
+    right=2.0
+    step=0.2
+    omega_values = np.arange(left,right,step) 
+    min_iterations_old = max_iter
+    for _ in tqdm(range(5)):
+      w_best=find_best_omega(omega_values, T, beta, max_iter, tolerance, results)
+      min_iterations = results[w_best]
+      if abs(min_iterations_old - min_iterations) < 2:
+        print(f"网格划分{_}次，w已收敛")
+        break
+      else:
+        min_iterations_old = min_iterations
+        print(min_iterations)
+      left = max(1.0, w_best - step)
+      right = min(2.0, w_best + step)
+      step=step/5
+      omega_values = np.arange(left, right, step)
+  
+    best_omega = min(results, key=results.get)
+    T_final = T.copy()
+    SOR(T_final, best_omega, beta, max_iter, tolerance)
+    print(f"Nx={Nx},Ny={Ny},Best ω = {best_omega}, iterations = {results[best_omega]}")
+    draw(Lx,Ly,Nx,Ny,T_final,best_omega)
+#调用
+for i in range(1, 5):
+    Nx=15*i
+    Ny=12*i
+    maincode(Nx,Ny) # 网格划分
+
 '''
 # --- Plotting 3D Surface ---
 fig = plt.figure(figsize=(10, 7))
